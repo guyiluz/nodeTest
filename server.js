855f54178595d79ed1a324ef2b817355b9f3a2a1
Guy home <guyi@hereo.co.uk>
2017-06-17 14:55:15 +0300

    test

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const  port = process.env.PORT || 8080
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
+var count =1
+
+setInterval(() => {
+  count++
+  fs.appendFile ('test.log', count + '/n', (err) =>{
+console.log("Unable to append to server.log")
+
+  })
+}, 1000);
+
 app.use((req, res, next)=>{
 
   var now = new Date().toString();
